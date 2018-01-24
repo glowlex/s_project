ab95c76dc0fca99c1b6d68f9dc86a5d0890ecfc0
glowlex <navsl@yandex.ru>
2018-01-25 02:54:07 +0300

    basic api

--- Node.cmd
+++ Node.cmd
@@ -1,1 +1,2 @@
-C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat" "&" cd /d %cd%\api\ raml2html api.raml > api.html 
+C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat" "&" cd /d %cd%\api\
+ raml2html api.raml > api.html
